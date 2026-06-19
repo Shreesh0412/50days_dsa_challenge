15ef43f88ee00e30adfef7b5ac8ddf5e67ef64d7
Shreesh0412 <shreesh.upa2007@gmail.com>
2026-06-19 22:30:30 +0530

    day16

---
+++ day_16/linked_list_cycle_2.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow=head
+        fast=head
+        hascycle=False
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                hascycle=True
+                break
+        if not hascycle:
+            return None
+        l=0
+        while slow.next!=fast:
+            slow=slow.next
+            l+=1
+        l+=1
+        slow=slow.next
+        slow=head
+        fast=head
+        for i in range(l):
+            fast=fast.next
+        while slow!=fast:
+            slow=slow.next
+            fast=fast.next
+        return slow
