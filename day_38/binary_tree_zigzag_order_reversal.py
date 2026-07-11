b18d323b259baccc6835c88b909bff3aa0788ae4
Shreesh0412 <shreesh.upa2007@gmail.com>
2026-07-11 21:24:21 +0530

    day38

---
+++ day_38/binary_tree_zigzag_order_reversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        from collections import deque
+        if not root:
+            return []
+        ans=[]
+        q=deque([root])
+        leftToRight=True
+        while q:
+            level=[]
+            for i in range(len(q)):
+                node=q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if not leftToRight:
+                level.reverse()
+            ans.append(level)
+            leftToRight=not leftToRight
+        return ans
