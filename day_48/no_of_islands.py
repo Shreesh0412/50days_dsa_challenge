bcd4dbfcd5fbb54173441ee347bbe82ae9b5998d
Shreesh0412 <shreesh.upa2007@gmail.com>
2026-07-21 20:50:09 +0530

    day48

---
+++ day_48/no_of_islands.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+        rows,cols=len(grid),len(grid[0])
+        def dfs(r,c):
+            if r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0":
+                return
+            grid[r][c]="0"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        islands=0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=="1":
+                    islands+=1
+                    dfs(i,j)
+        return islands
